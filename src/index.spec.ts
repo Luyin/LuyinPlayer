d36f3f2dc032ad54bdef81af9269922412829dbf
Luyin <myraous@gmail.com>
2019-05-06 18:06:27 +0900

    chore: initial development environment

---
+++ src/index.spec.ts
@@ -0,0 +1,12 @@
+import LuyinPlayer from './index';
+
+describe('Luyin Player', () => {
+    describe('#Play', () => {
+        it('Initialize', () => {
+           const options = {
+
+           };
+           const luyinPlayer = new LuyinPlayer('luyin-dom', options);
+        });
+    });
+});
---
+++ src/index.ts
@@ -0,0 +1,11 @@
+class LuyinPlayer {
+    private selector: string;
+    private options: object;
+
+    constructor(selector: string, options: object) {
+        this.selector = selector;
+        this.options = options;
+    }
+}
+
+export default LuyinPlayer;
